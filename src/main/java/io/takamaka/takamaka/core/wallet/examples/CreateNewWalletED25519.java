7cc727ec6f55c3b28485fcff5c8755f9b85e49ed
zeikeneim <alessandro.pasi@takamaka.io>
2023-01-16 18:23:53 +0100

    added new example of stake undo

--- src/main/java/io/takamaka/takamaka/core/wallet/examples/CreateNewWalletED25519.java
+++ src/main/java/io/takamaka/takamaka/core/wallet/examples/CreateNewWalletED25519.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
  * @author giovanni
  */
 @Slf4j
-public class CreateNewWalletBCED25519 {
+public class CreateNewWalletED25519 {
 
     public static void main(String[] args) throws Exception {
         //generic interface for all takamaka wallet
